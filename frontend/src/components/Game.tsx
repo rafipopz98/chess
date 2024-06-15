0810c073f9064522f0cf6c15c38c19496a6bae1b
rafipopz98 <itsrealrafi@gmail.com>
2024-06-16 00:53:26 +0530

    never used values

--- frontend/src/components/Game.tsx
+++ frontend/src/components/Game.tsx
@@ -11,7 +11,7 @@ export const GAME_OVER = "game_over";
 
 const Game = () => {
   const socket = useSocket();
-  const [chess, setChess] = useState(new Chess());
+  const [chess, _] = useState(new Chess());
   const [board, setBoard] = useState(chess.board());
   const [started, setStarted] = useState(false);
 
